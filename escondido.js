d99b049c1c68990c23546862408fa037c3c03e6a
ronogure <59735993+ronogure@users.noreply.github.com>
2020-07-01 15:42:15 -0300

    Create escondido.js

---
+++ escondido.js
@@ -0,0 +1,13 @@
+var rd-text-kc3i4vmz;
+window.onload = function(){
+        rd-text-kc3i4vmz = document.getElementById("rd-text-kc3i4vmz")
+        var rd-image-kc3f5qbh = document.getElementById("rd-image-kc3f5qbh")
+        rd-image-kc3f5qbh.onclick = mostrarRd-text-kc3i4vmz
+        
+        rd-text-kc3i4vmz.classList.add("hidden");
+        
+}
+    function mostraraereas(){
+        rd-text-kc3i4vmz.classList.remove("hidden");
+        rd-text-kc3i4vmz.classList.add("hidden"); 
+    }
